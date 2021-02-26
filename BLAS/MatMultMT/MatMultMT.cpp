867c50e2ac387ef95182e9db3fc0b9ba8a3c54c6
LRLVEC <mjdyx@outlook.com>
2021-02-26 23:14:28 +0800

    finished matmultMT

--- BLAS/MatMultMT/MatMultMT.cpp
+++ BLAS/MatMultMT/MatMultMT.cpp
@@ -8,6 +8,250 @@
 #include <pthread.h>
 #include <sys/sysinfo.h>
 #endif
+BLAS::mat& matMultMT_(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
+{
+	using namespace BLAS;
+	unsigned long long threadNum;
+#ifdef _WIN32
+	SYSTEM_INFO systemInfo;
+	GetSystemInfo(&systemInfo);
+	threadNum = systemInfo.dwNumberOfProcessors;
+#else
+	threadNum = get_nprocs_conf();
+#endif
+	unsigned long long minDim(ts.width > a.height ? a.height : ts.width);
+	if (minDim)
+	{
+		bool overflow(ceiling4(a.width, ts.height) > b.width4d * b.height);
+		if (overflow && b.type != Type::Native)return b;
+		mat const* source(&ts);
+		mat r;
+		if (&b == &ts)
+		{
+			source = &r;
+			r = ts;
+		}
+		if (overflow)b.reconstruct(a.width, source->height, false);
+		__m256d* aData((__m256d*)a.data);
+		__m256d* bData((__m256d*)b.data);
+		constexpr unsigned long long warp = 16;
+		unsigned long long aWidth256d(a.width4d / 4);
+		unsigned long long aWidthWarpFloor((aWidth256d / warp) * warp);
+		unsigned long long warpLeft(aWidth256d - aWidthWarpFloor);
+		unsigned long long height2Floor(source->height & (-2));
+
+		//ptr: A beginning
+		//     A ending
+		//     B
+		//     C beginning
+		//     width4d
+		//     aWidthWarpFloor
+		//     minDim
+		//     aWidth256d
+		//     warpLeft
+#ifdef _WIN32
+		void (*lambda)(void*) = [](void* ptr)
+#else
+		void* (*lambda)(void*) = [](void* ptr)->void*
+#endif
+		{
+			void** ptrs((void**)ptr);
+			double* source = ((double*)ptrs[0]);
+			double* sourceEnding((double*)ptrs[1]);
+			__m256d* aData((__m256d*)ptrs[2]);
+			__m256d* bData((__m256d*)ptrs[3]);
+			unsigned long long width4d((unsigned long long)ptrs[4]);
+			unsigned long long aWidthWarpFloor((unsigned long long)ptrs[5]);
+			unsigned long long minDim((unsigned long long)ptrs[6]);
+			unsigned long long aWidth256d((unsigned long long)ptrs[7]);
+			unsigned long long warpLeft((unsigned long long)ptrs[8]);
+
+			constexpr unsigned long long warp = 16;
+			for (; source < sourceEnding; source += width4d * 2, bData += aWidth256d * 2)
+			{
+				unsigned long long c1(0);
+				for (; c1 < aWidthWarpFloor; c1 += warp)
+				{
+					__m256d ans0[warp] = { 0 };
+					__m256d ans1[warp] = { 0 };
+					for (unsigned long long c2(0); c2 < minDim; ++c2)
+					{
+						__m256d tp0 = _mm256_set1_pd(source[c2]);
+						__m256d tp1 = _mm256_set1_pd(source[width4d + c2]);
+#pragma unroll(4)
+						for (unsigned long long c3(0); c3 < warp; ++c3)
+						{
+							__m256d b = aData[aWidth256d * c2 + c1 + c3];
+							ans0[c3] = _mm256_fmadd_pd(tp0, b, ans0[c3]);
+							ans1[c3] = _mm256_fmadd_pd(tp1, b, ans1[c3]);
+						}
+					}
+#pragma unroll(4)
+					for (unsigned long long c3(0); c3 < warp; ++c3)
+					{
+						bData[c1 + c3] = ans0[c3];
+						bData[aWidth256d + c1 + c3] = ans1[c3];
+					}
+				}
+				if (c1 < aWidth256d)
+				{
+					__m256d ans0[warp] = { 0 };
+					__m256d ans1[warp] = { 0 };
+					for (unsigned long long c2(0); c2 < minDim; ++c2)
+					{
+						__m256d tp0 = _mm256_set1_pd(source[c2]);
+						__m256d tp1 = _mm256_set1_pd(source[width4d + c2]);
+						for (unsigned long long c3(0); c3 < warpLeft; ++c3)
+						{
+							__m256d b = aData[aWidth256d * c2 + c1 + c3];
+							ans0[c3] = _mm256_fmadd_pd(tp0, b, ans0[c3]);
+							ans1[c3] = _mm256_fmadd_pd(tp1, b, ans1[c3]);
+						}
+					}
+					for (unsigned long long c3(0); c3 < warpLeft; ++c3)
+					{
+						bData[c1 + c3] = ans0[c3];
+						bData[aWidth256d + c1 + c3] = ans1[c3];
+					}
+				}
+			}
+#ifndef _WIN32
+			return 0;
+#endif
+		};
+
+		unsigned long long threadHeight;
+		unsigned long long rowBeginning;
+		unsigned long long leftHeight;
+		if (source->height / threadNum >= 2)
+		{
+			threadHeight = (source->height / threadNum) & (-2);
+		}
+		else
+		{
+			threadHeight = 2;
+			threadNum = source->height / 2;
+		}
+		rowBeginning = threadHeight * threadNum;
+		leftHeight = source->height - rowBeginning;
+
+#ifdef _WIN32
+		HANDLE* threads(nullptr);
+#else
+		pthread_t* threads(nullptr);
+#endif
+		void** paras(nullptr);
+		if (threadNum)
+		{
+#ifdef _WIN32
+			threads = (HANDLE*)::malloc(threadNum * sizeof(HANDLE));
+#else
+			threads = (pthread_t*)::malloc(threadNum * sizeof(pthread_t));
+#endif
+			paras = (void**)::malloc(threadNum * 9 * sizeof(void*));
+			for (unsigned long long c0(0); c0 < threadNum; ++c0)
+			{
+				paras[c0 * 9] = (void*)(source->data + threadHeight * source->width4d * c0);			//A beginning
+				paras[c0 * 9 + 1] = (void*)(source->data + threadHeight * source->width4d * (c0 + 1));	//A ending
+				paras[c0 * 9 + 2] = (void*)(a.data);													//B
+				paras[c0 * 9 + 3] = (void*)(b.data + threadHeight * a.width4d * c0);					//C beginning
+				paras[c0 * 9 + 4] = (void*)(source->width4d);											//width4d
+				paras[c0 * 9 + 5] = (void*)(aWidthWarpFloor);											//aWidthWarpFloor
+				paras[c0 * 9 + 6] = (void*)(minDim);													//minDim
+				paras[c0 * 9 + 7] = (void*)(aWidth256d);												//aWidth256d
+				paras[c0 * 9 + 8] = (void*)(warpLeft);													//warpLeft
+#ifdef _WIN32
+				threads[c0] = (HANDLE)_beginthread(lambda, 0, paras + c0 * 9);
+#else
+				pthread_create(threads + c0, 0, lambda, paras + c0 * 9);
+				pthread_detach(threads[c0]);
+#endif
+			}
+#ifdef _WIN32
+			DWORD rc = WaitForMultipleObjects(threadNum, threads, true, INFINITE);
+			for (unsigned long long c0(0); c0 < threadNum; ++c0)
+				CloseHandle(threads[c0]);
+#else
+			for (unsigned long long c0(0); c0 < threadNum; ++c0)
+				pthread_join(threads[c0], nullptr);
+#endif
+		}
+		if (leftHeight > 1)
+		{
+			threadHeight = 2;
+			threadNum = leftHeight / 2;
+			for (unsigned long long c0(0); c0 < threadNum; ++c0)
+			{
+				paras[c0 * 9] = (void*)(source->data + source->width4d * (threadHeight * c0 + rowBeginning));			//A beginning
+				paras[c0 * 9 + 1] = (void*)(source->data + source->width4d * (threadHeight * (c0 + 1) + rowBeginning));	//A ending
+				paras[c0 * 9 + 2] = (void*)(a.data);																	//B
+				paras[c0 * 9 + 3] = (void*)(b.data + a.width4d * (threadHeight * c0 + rowBeginning));					//C beginning
+				paras[c0 * 9 + 4] = (void*)(source->width4d);															//width4d
+				paras[c0 * 9 + 5] = (void*)(aWidthWarpFloor);															//aWidthWarpFloor
+				paras[c0 * 9 + 6] = (void*)(minDim);																	//minDim
+				paras[c0 * 9 + 7] = (void*)(aWidth256d);																//aWidth256d
+				paras[c0 * 9 + 8] = (void*)(warpLeft);																	//warpLeft
+#ifdef _WIN32
+				threads[c0] = (HANDLE)_beginthread(lambda, 0, paras + c0 * 9);
+#else
+				pthread_create(threads + c0, 0, lambda, paras + c0 * 9);
+				pthread_detach(threads[c0]);
+#endif
+			}
+#ifdef _WIN32
+			DWORD rc = WaitForMultipleObjects(threadNum, threads, true, INFINITE);
+			for (unsigned long long c0(0); c0 < threadNum; ++c0)
+				CloseHandle(threads[c0]);
+#else
+			for (unsigned long long c0(0); c0 < threadNum; ++c0)
+				pthread_join(threads[c0], nullptr);
+#endif
+			rowBeginning += threadHeight * threadNum;
+		}
+		free(threads);
+		free(paras);
+		if (source->height & 1)
+		{
+			double* sourceBeginning(source->data + rowBeginning * source->width4d);
+			bData += rowBeginning * aWidth256d;
+
+			unsigned long long c1(0);
+			for (; c1 < aWidthWarpFloor; c1 += warp)
+			{
+				__m256d ans0[warp] = { 0 };
+				for (unsigned long long c2(0); c2 < minDim; ++c2)
+				{
+					__m256d tp0 = _mm256_set1_pd(sourceBeginning[c2]);
+#pragma unroll(4)
+					for (unsigned long long c3(0); c3 < warp; ++c3)
+					{
+						__m256d b = aData[aWidth256d * c2 + c1 + c3];
+						ans0[c3] = _mm256_fmadd_pd(tp0, b, ans0[c3]);
+					}
+				}
+#pragma unroll(4)
+				for (unsigned long long c3(0); c3 < warp; ++c3)
+					bData[c1 + c3] = ans0[c3];
+			}
+			if (c1 < aWidth256d)
+			{
+				__m256d ans0[warp] = { 0 };
+				for (unsigned long long c2(0); c2 < minDim; ++c2)
+				{
+					__m256d tp0 = _mm256_set1_pd(sourceBeginning[c2]);
+					for (unsigned long long c3(0); c3 < warpLeft; ++c3)
+					{
+						__m256d b = aData[aWidth256d * c2 + c1 + c3];
+						ans0[c3] = _mm256_fmadd_pd(tp0, b, ans0[c3]);
+					}
+				}
+				for (unsigned long long c3(0); c3 < warpLeft; ++c3)
+					bData[c1 + c3] = ans0[c3];
+			}
+		}
+	}
+	return b;
+}
 
 BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 {
@@ -286,9 +530,9 @@ int main()
 	Timer timer;
 	using namespace BLAS;
 	// 3950x avx2 multi thread test
-	mat mA(1024, 1024, false);
-	mat mB(1024, 1024, false);
-	mat mC(1024, 1024, false);
+	mat mA(1023, 1023, false);
+	mat mB(1023, 1023, false);
+	mat mC(1023, 1023, false);
 	randomMat(mA, mt, rd);
 	randomMat(mB, mt, rd);
 	mA.printToTableTxt("./matA.txt");
@@ -302,7 +546,7 @@ int main()
 	timer.begin();
 	for (unsigned long long c0(0); c0 < 1; ++c0)
 		//mA(mB, mC);
-		matMultMT(mA, mB, mC);
+		matMultMT_(mA, mB, mC);
 	timer.end();
 	timer.print("Multi Thread: ");
 	mC.printToTableTxt("./matC.txt");
