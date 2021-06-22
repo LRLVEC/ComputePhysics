a5f725364bbf3d4e92198df7520bdd214cee539e
LRLVEC <mjdyx@outlook.com>
2021-06-23 00:41:51 +0800

    Update AVX2 mat mult

--- BLAS/MatMultMT/MatMultMT.cpp
+++ BLAS/MatMultMT/MatMultMT.cpp
@@ -67,34 +67,44 @@ BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 			unsigned long long aWidth256d((unsigned long long)ptrs[7]);
 			unsigned long long warpLeft((unsigned long long)ptrs[8]);
 
-			constexpr unsigned long long warp = 16;
-			for (; source < sourceEnding; source += width4d * 2, bData += aWidth256d * 2)
+			constexpr unsigned long long warp = 2;
+			constexpr unsigned long long group = 4;
+			for (; source < sourceEnding; source += width4d * group, bData += aWidth256d * group)
 			{
 				unsigned long long c1(0);
 				for (; c1 < aWidthWarpFloor; c1 += warp)
 				{
 					__m256d ans0[warp] = { 0 };
 					__m256d ans1[warp] = { 0 };
+					__m256d ans2[warp] = { 0 };
+					__m256d ans3[warp] = { 0 };
 					for (unsigned long long c2(0); c2 < minDim; ++c2)
 					{
 						__m256d tp0 = _mm256_set1_pd(source[c2]);
 						__m256d tp1 = _mm256_set1_pd(source[width4d + c2]);
-#pragma unroll(4)
-						for (unsigned long long c3(0); c3 < warp; ++c3)
-						{
-							__m256d b = aData[aWidth256d * c2 + c1 + c3];
-							ans0[c3] = _mm256_fmadd_pd(tp0, b, ans0[c3]);
-							ans1[c3] = _mm256_fmadd_pd(tp1, b, ans1[c3]);
-						}
+						__m256d tp2 = _mm256_set1_pd(source[2 * width4d + c2]);
+						__m256d tp3 = _mm256_set1_pd(source[3 * width4d + c2]);
+						__m256d a = aData[aWidth256d * c2 + c1];
+						__m256d b = aData[aWidth256d * c2 + c1 + 1];
+						ans0[0] = _mm256_fmadd_pd(tp0, a, ans0[0]);
+						ans0[1] = _mm256_fmadd_pd(tp0, b, ans0[1]);
+						ans1[0] = _mm256_fmadd_pd(tp1, a, ans1[0]);
+						ans1[1] = _mm256_fmadd_pd(tp1, b, ans1[1]);
+						ans2[0] = _mm256_fmadd_pd(tp2, a, ans2[0]);
+						ans2[1] = _mm256_fmadd_pd(tp2, b, ans2[1]);
+						ans3[0] = _mm256_fmadd_pd(tp3, a, ans3[0]);
+						ans3[1] = _mm256_fmadd_pd(tp3, b, ans3[1]);
 					}
 #pragma unroll(4)
 					for (unsigned long long c3(0); c3 < warp; ++c3)
 					{
 						bData[c1 + c3] = ans0[c3];
 						bData[aWidth256d + c1 + c3] = ans1[c3];
+						bData[2 * aWidth256d + c1 + c3] = ans2[c3];
+						bData[3 * aWidth256d + c1 + c3] = ans3[c3];
 					}
 				}
-				if (c1 < aWidth256d)
+				/*if (c1 < aWidth256d)
 				{
 					__m256d ans0[warp] = { 0 };
 					__m256d ans1[warp] = { 0 };
@@ -114,7 +124,7 @@ BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 						bData[c1 + c3] = ans0[c3];
 						bData[aWidth256d + c1 + c3] = ans1[c3];
 					}
-				}
+				}*/
 			}
 			return 0;
 		};
@@ -122,14 +132,14 @@ BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 		unsigned long long threadHeight;
 		unsigned long long rowBeginning;
 		unsigned long long leftHeight;
-		if (source->height / threadNum >= 2)
+		if (source->height / threadNum >= 4)
 		{
-			threadHeight = (source->height / threadNum) & (-2);
+			threadHeight = (source->height / threadNum) & (-4);
 		}
 		else
 		{
-			threadHeight = 2;
-			threadNum = source->height / 2;
+			threadHeight = 4;
+			threadNum = source->height / 4;
 		}
 		rowBeginning = threadHeight * threadNum;
 		leftHeight = source->height - rowBeginning;
@@ -175,7 +185,7 @@ BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 				pthread_join(threads[c0], nullptr);
 #endif
 		}
-		if (leftHeight > 1)
+		/*if (leftHeight > 1)
 		{
 			threadHeight = 2;
 			threadNum = leftHeight / 2;
@@ -206,10 +216,10 @@ BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 				pthread_join(threads[c0], nullptr);
 #endif
 			rowBeginning += threadHeight * threadNum;
-		}
+		}*/
 		free(threads);
 		free(paras);
-		if (source->height & 1)
+		/*if (source->height & 1)
 		{
 			double* sourceBeginning(source->data + rowBeginning * source->width4d);
 			bData += rowBeginning * aWidth256d;
@@ -247,7 +257,7 @@ BLAS::mat& matMultMT(BLAS::mat const& ts, BLAS::mat const& a, BLAS::mat& b)
 				for (unsigned long long c3(0); c3 < warpLeft; ++c3)
 					bData[c1 + c3] = ans0[c3];
 			}
-		}
+		}*/
 	}
 	return b;
 }
@@ -257,15 +267,15 @@ int main()
 	std::uniform_real_distribution<double> rd(-1.0, 1.0);
 	std::uniform_int_distribution<unsigned long long> rduint(1, 10);
 
-	//remove("./matA.txt");
-	//remove("./matB.txt");
-	//remove("./matC.txt");
+	remove("./matA.txt");
+	remove("./matB.txt");
+	remove("./matC.txt");
 
 	Timer timer;
 	using namespace BLAS;
 	// 3950x avx2 multi thread test
 
-	for (unsigned int dim(1024); dim < 2050; ++dim)
+	/*for (unsigned int dim(1024); dim < 2050; ++dim)
 	{
 		mat mA(dim, dim, false);
 		mat mB(dim, dim, false);
@@ -281,9 +291,9 @@ int main()
 		//	eps += mC.row(c0).norm2();
 		//if (eps > 0)
 		//	printf("\r%4u: %.6e\n", dim, eps);
-		//else 
+		//else
 		printf("\r%4u", dim);
-	}
+	}*/
 
 	/*_beginthreadex_proc_type lambda = //(void(__stdcall*)(void*))
 		[](void* ptr)->unsigned
@@ -299,24 +309,24 @@ int main()
 	WaitForSingleObjectEx(thread, INFINITE, false);
 	CloseHandle(thread);*/
 
-	//mat mA(1023, 1023, false);
-	//mat mB(1023, 1023, false);
-	//mat mC(1023, 1023, false);
-	//randomMat(mA, mt, rd);
-	//randomMat(mB, mt, rd);
-	//mA.printToTableTxt("./matA.txt");
-	//mB.printToTableTxt("./matB.txt");
+	mat mA(1024, 1024, false);
+	mat mB(1024, 1024, false);
+	mat mC(1024, 1024, false);
+	randomMat(mA, mt, rd);
+	randomMat(mB, mt, rd);
 
 	//timer.begin();
 	//mA(mB, mC);
 	//timer.end();
 	//timer.print("Single Thread: ");
 
-	//timer.begin();
-	//for (unsigned long long c0(0); c0 < 1; ++c0)
-	//	//mA(mB, mC);
-	//	matMultMT_(mA, mB, mC);
-	//timer.end();
-	//timer.print("Multi Thread: ");
+	timer.begin();
+	for (unsigned long long c0(0); c0 < 10; ++c0)
+		//mA(mB, mC);
+		matMultMT(mA, mB, mC);
+	timer.end();
+	timer.print("3950X AVX2 Multi Thread: ");
+	//mA.printToTableTxt("./matA.txt");
+	//mB.printToTableTxt("./matB.txt");
 	//mC.printToTableTxt("./matC.txt");
 }
